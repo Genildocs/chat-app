e2c816f43d4b6f06cd495c363ca13132139ebc1b
genildocs <genildocs@gmail.com>
2024-06-04 03:42:04 -0300

    commit

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,3 +1,20 @@
 export default function Chat() {
-  return <div className=" flex-1">Chat</div>;
+  return (
+    <div className="chat h-full">
+      <div className=" p-5 iFlex justify-between">
+        <div className="iFlex gap-5">
+          <div className="e-avatar e-avatar-large rounded-full">JD</div>
+          <div className="flex flex-col gap-1">
+            <span className=" text-lg font-bold">Jane doe</span>
+            <p className="text-sm font-light">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.{' '}
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-5">icons</div>
+      </div>
+      <div className="">center</div>
+      <div className="">bottom</div>
+    </div>
+  );
 }
